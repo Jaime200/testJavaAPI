a974e42b4adb55b91275d1814f2b719aaf812702
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-08-01 11:37:42 -0600

    Parametrizacion de los metodos

--- src/main/java/com/example/apiRestTest/apiRestTest/useCase/getInfoServicesImplement.java
+++ src/main/java/com/example/apiRestTest/apiRestTest/useCase/getInfoServicesImplement.java
@@ -4,17 +4,8 @@ import com.example.apiRestTest.apiRestTest.Entity.ResultItunes;
 import com.example.apiRestTest.apiRestTest.Entity.Tvmaze;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.sun.source.tree.TryTree;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,16 +27,6 @@ public class getInfoServicesImplement implements getInfoServices{
             throw new Exception( e.getMessage(),e.getCause());
         }
 
-
-
-       /* Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://itunes.apple.com/search").queryParam("term", termino);
-        Response response = target.request(MediaType.TEXT_PLAIN).get();
-        String strItunesArtist = response.readEntity(String.class);
-        Gson gson = new Gson();
-        ResultItunes arrItunesArtist =  gson.fromJson(strItunesArtist, ResultItunes.class);
-
-        return arrItunesArtist;*/
     }
 
     @Override
@@ -56,7 +37,7 @@ public class getInfoServicesImplement implements getInfoServices{
     @Override
     public List<Tvmaze> infoTvmaze(String termino) throws Exception {
 
-            if(termino.length() >0){
+
                 final String uri = "http://api.tvmaze.com/search/shows?q={termino}";
                 try {
                     RestTemplate restTemplate = new RestTemplate();
@@ -73,29 +54,5 @@ public class getInfoServicesImplement implements getInfoServices{
                 }
 
 
-                /*try {
-                Client client = ClientBuilder.newClient();
-                WebTarget target = client.target("http://api.tvmaze.com/search/shows").queryParam("q", termino);
-                Response response = target.request(MediaType.TEXT_PLAIN).get();
-                String strTvmaze = response.readEntity(String.class);
-                Gson gson = new Gson();
-                java.lang.reflect.Type listType = new TypeToken<List<Tvmaze>>()
-                {
-                }.getType();
-
-                List<Tvmaze> arrTvmaze = new ArrayList<Tvmaze>();
-                arrTvmaze =  gson.fromJson(strTvmaze, listType);
-                //http://api.tvmaze.com/search/shows?q=girls
-
-                return arrTvmaze;
-                } catch (Exception e) {
-                    return null;
-                }*/
-            }
-
-
-
-
-        return null;
     }
 }
