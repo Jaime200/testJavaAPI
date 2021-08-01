ea733fae15bb7ef6b2f82bb93779a35b08e0401c
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-07-31 20:48:18 -0600

    Se modifica entidad para que acepte ResultItunes

--- src/main/java/com/example/apiRestTest/apiRestTest/useCase/getInfoServices.java
+++ src/main/java/com/example/apiRestTest/apiRestTest/useCase/getInfoServices.java
@@ -1,13 +1,14 @@
 package com.example.apiRestTest.apiRestTest.useCase;
 
-import com.example.apiRestTest.apiRestTest.Entity.ItunesArtist;
+
+import com.example.apiRestTest.apiRestTest.Entity.ResultItunes;
 import com.example.apiRestTest.apiRestTest.Entity.Tvmaze;
 
 import java.util.List;
 
 public interface getInfoServices {
 
-    List<ItunesArtist> infoArtists(String termino);
+    List<ResultItunes> infoArtists(String termino);
     void infoPersonas(String termino);
     List<Tvmaze> infoTvmaze(String termino);
 }
